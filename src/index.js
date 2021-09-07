837d81e5e6ae2e2d2a09b36d167e3ff34d034b05
Fredy <fredgerman30@gmail.com>
2021-09-07 17:59:27 +0300

    appended filters and brands to url

--- src/index.js
+++ src/index.js
@@ -1,37 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import rootReducer from './reducers'
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { routerMiddleware, push } from 'connected-react-router'
-import { createBrowserHistory } from 'history';
-import createSagaMiddleware from 'redux-saga';
-import { getItemPath } from './util/path-utils';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./reducers";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { routerMiddleware, push } from "connected-react-router";
+import { createBrowserHistory } from "history";
+import createSagaMiddleware from "redux-saga";
+import { getItemPath } from "./util/path-utils";
 
-import mySaga from './saga';
+import mySaga from "./saga";
 
-export const history = createBrowserHistory()
+export const history = createBrowserHistory();
 
-const middleware = store => next => action => {
-    if (action.type === "ADD_ITEM") {
-        const state = store.getState();
-        store.dispatch(push(`${state.router.location.pathname}?${getItemPath([...state.items, action.data])}`));
-    }
-    next(action);
-}
+const middleware = (store) => (next) => (action) => {
+  if (action.type === "ADD_ITEM") {
+    const state = store.getState();
+    console.log(state);
+    store.dispatch(
+      push(
+        `${state.router.location.pathname}?${getItemPath([...state.items,action.data,
+        ])}`
+      )
+    );
+  }
+  next(action);
+};
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(rootReducer(history), applyMiddleware(sagaMiddleware, middleware, routerMiddleware(history)));
+const store = createStore(
+  rootReducer(history),
+  applyMiddleware(sagaMiddleware, middleware, routerMiddleware(history))
+);
 
 sagaMiddleware.run(mySaga);
 
 store.dispatch({ type: "INIT_APP" });
 
-ReactDOM.render(<Provider store={store}>
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-</Provider>, document.getElementById('root'));
+  </Provider>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
